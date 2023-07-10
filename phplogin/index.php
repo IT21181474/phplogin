bac4891d04ba83dcf1ff109c7f480872f9da3431
Yashini Hettiarachchi <99383400+IT21181474@users.noreply.github.com>
2023-07-10 22:48:40 +0530

    Add files

---
+++ phplogin/index.php
@@ -0,0 +1,51 @@
+<?php
+    include("connection.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div id="form">
+    <h1>Login Form</h1>
+        <form name="form" action="login.php" onsubmit="return invalid()" method="POST">
+            <label>Username : </label>
+            <input type="text" name="user" id="user"><br><br>
+            <label>Password : </label>
+            <input type="password" name="pass" id="pass"><br><br>
+            <input type="submit" id="btn" value="Login" name="submit">
+        
+       
+
+        </form>
+    </div>       
+    <script>
+        function invalid(){
+            var user=document.form.user.value;
+            var pass=document.form.pass.value;
+            if(user.length=="" && pass.length==""){
+                alert("username and password filed is empty");
+                return false
+
+            }
+            else{
+                if(user.length==""){
+                    alert("username is empty");
+                    return false
+
+                }
+                if(pass.length==""){
+                    alert("password is empty");
+                    return false
+
+                }
+            }
+        }
+    </script>
+    
+</body>
+</html>
